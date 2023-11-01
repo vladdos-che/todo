a4526d4087286ffb660e4c531e5b58671fc1c0c4
vlad-che <vlad1010che@gmail.com>
2023-11-01 16:47:35 +0600

    lesson_56 end project

--- todo/urls.py
+++ todo/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-# from todolist.views import redirect_view, todo, category
+from todolist.views import redirect_view, todo, category
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # path('', redirect_view),
-    # path('todo/', todo, name='todo'),
-    # path('category/', category, name='category'),
+    path('', redirect_view),
+    path('todo/', todo, name='todo'),
+    path('category/', category, name='category'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import TodoList, Category
+
+
+def redirect_view(request):
+    return redirect('category/')
+
+
+def todo(request):
+    todos = TodoList.objects.all()
+    categories = Category.objects.all()
+
+    if request.method == "POST":
+        if "Add" in request.POST:
+            title = request.POST["description"]
+            date = str(request.POST["date"])
+            category = request.POST["category_select"]
+            content = title + " -- " + date + " " + category
+            todo = TodoList(title=title, content=content, due_date=date,
+                            category=Category.objects.get(name=category))
+            todo.save()
+            return redirect("/todo")
+
+        if "Delete" in request.POST:
+            checked_list = request.POST.getlist('checkedbox')
+
+            for i in range(len(checked_list)):
+                todo = TodoList.objects.filter(id=int(checked_list[i]))
+                todo.delete()
+
+    return render(request, "todo.html",
+                  {"todos": todos, "categories": categories})
+
+
+def category(request):
+    categories = Category.objects.all()
+
+    if request.method == "POST":
+        if "Add" in request.POST:
+            name = request.POST["name"]
+            category = Category(name=name)
+            category.save()
+            return redirect("/category")
+
+        if "Delete" in request.POST:
+            check = request.POST.getlist('check')
+
+            for i in range(len(check)):
+                try:
+                    categ = Category.objects.filter(id=int(check[i]))
+                    categ.delete()
+                except BaseException:
+                    return HttpResponse('<h1>Сначала удалите карточки с этими категориями!</h1>')
+
+    return render(request, "category.html", {"categories": categories})
